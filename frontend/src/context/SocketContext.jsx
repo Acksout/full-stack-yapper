9d097913360d6cd8829cdd1246c618c8bde72b15
Acksout <157364563+Acksout@users.noreply.github.com>
2024-04-02 19:41:14 +0530

    Update SocketContext.jsx

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -17,7 +17,7 @@ export const SocketContextProvider = ({children}) => {
 
     useEffect(() => {
         if (authUser) {
-            const socket = io("https://mern-yapper.vercel.app", {
+            const socket = io("https://full-stack-yapper.onrender.com", {
                 query: {
                     userId: authUser._id,
                 },
