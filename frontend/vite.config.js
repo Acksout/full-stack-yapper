3aade9fe9dda418941446529c46905909ccf849e
Acksout <157364563+Acksout@users.noreply.github.com>
2024-04-02 19:35:34 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
         port: 3000,
         proxy: {
             "/api": {
-                target: "https://mern-yapper.vercel.app",
+                target: "http://localhost:8000",
             },
         },
     },
